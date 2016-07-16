84a2398d70cbc25d968a6107dc02368ed2491164
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-07-16 12:37:36 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,21 @@
+public class PositiveNo {
+
+    
+    public static void main(String[] args) {
+       int n;
+       Scanner sc =new Scanner(System.in);
+       i=sc.nextInt();
+       if(n>0)
+       {
+           System.out.println("Positive no");
+       } else if(n==0)
+       {
+           System.out.println("Zero");
+       }
+       else 
+               {
+                   System.out.println("Negative no");
+               }
+    }
+
+}

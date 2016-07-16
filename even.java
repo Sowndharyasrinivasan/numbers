87475152ad009322ad5a48e1aec9d75e7355e015
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-07-16 11:41:10 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,19 @@
+public class Evenodd {
+
+   
+    public static void main(String[] args) {
+        int i = 0,n;
+        System.out.println("enter the no");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        
+        if(i%2==0)
+        {
+            System.out.println("the no is even");  
+        }
+        else
+        {
+            System.out.println("the no is odd");
+        }
+    }
+}

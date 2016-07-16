a6bfd2a0f2bcc8311d30d6aa68a14610fbe5cf8b
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-07-16 12:40:39 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+public class Multiplication {
+
+   
+    public static void main(String[] args) {
+        int i,j,b;
+        System.out.println("enter the no of table");
+        Scanner sc=new Scanner(System.in);
+        i=sc.nextInt();
+        for(j=1;j<=10;j++)
+        {
+            b=i*j;
+            System.out.println(j+"*"+i+"="+k+"");
+                    
+        }
+    }
+}

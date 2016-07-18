aa956b798eb206c2184c04c3b664a7a76d4e78e9
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-07-18 17:21:36 +0530

    Add files via upload

---
+++ Factorials.java
@@ -0,0 +1,23 @@
+package factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+   
+    public static void main(String[] args) {
+       int n=5,i;
+       int fact=1;
+       
+        Scanner sc=new Scanner(System.in);
+         System.out.println("enter the factorial no:");
+        n=sc.nextInt();
+       
+        for(i=1;i<=n;i++)
+               fact=fact*i;
+        {
+            System.out.println("the factorial" +n+ "is" +fact);
+        }
+    }
+    }
+

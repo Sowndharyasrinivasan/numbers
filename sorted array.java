f113c17c411f09145220b35d22d6128d7dbc886e
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-07-21 15:52:01 +0530

    Add files via upload

---
+++ sorted array.java
@@ -0,0 +1,15 @@
+public static void main(String ar[])throws Exception
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int ar[]=new int[n];
+for(i=0;i<n;i++)
+ar[i]=sc.nextInt();
+for(i=0;i<n;i++)
+{
+if(ar[i]==i)
+System.out.println("the number"+ar[i]+"is equals to its index"+i);
+}
+}
+}

854400cdc13430108e93836e9ae5fb948de30a66
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-07-22 15:44:54 +0530

    Add files via upload

--- contributor1.java
+++ contributor1.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 
 public class JavaApplication5 {
+
+
 public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
+
+       Scanner s=new Scanner(System.in);
+
        System.out.println("enter n value");
-        int n=sc.nextInt(); int K=1;
-	for(int i=0;i<2*n;i++)	
-        {int x=2*n-i;
-	  if(n!=x) 
+        int a=s.nextInt();
+     int K=1;
+	for(int i=0;i<2*a;i++)	
+        {
+      int x=2*a-i;
+	  if(a!=x) 
            {
              System.out.println(" "+K++ +"  "+"  "+K);  
               }		
